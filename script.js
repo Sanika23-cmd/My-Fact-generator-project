3d162c678b0b0893de9c73b73718061db1185506
Sanika23-cmd <sanikaparanjape23@gmail.com>
2024-11-15 18:24:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('generateBtn').addEventListener('click', function() {
+    const facts = [
+        "You enjoy winter activities and the beauty of snow.",
+        "You love socializing and attending lively
+        </MESSAGE>
